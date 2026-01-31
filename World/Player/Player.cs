f32236698029503b622d6186c4eb6b907a66695e
Talberon <developer@talberon.com>
2026-01-30 20:58:16 -0400

    WIP Input definitions

--- World/Player/Player.cs
+++ World/Player/Player.cs
@@ -3,16 +3,17 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-	[Export] private AnimationPlayer animationPlayer;
+    [Export] private AnimationPlayer animationPlayer;
 
-	public override void _Ready()
-	{
-		base._Ready();
-	}
+    public override void _Ready()
+    {
+        base._Ready();
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		//TODO: Game logic
-		base._PhysicsProcess(delta);
-	}
+    public override void _PhysicsProcess(double delta)
+    {
+        var horizontalMove = Input.GetAxis("move_left", "move_right");
+        //TODO: Game logic
+        base._PhysicsProcess(delta);
+    }
 }
