81de45218cd2b883eba196fc1955893876ab0606
Talberon <developer@talberon.com>
2026-01-31 18:42:21 -0400

    Scaffolding cutscene orchestrator

---
+++ World/System/CutsceneOrchestrator.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class CutsceneOrchestrator : Node3D
+{
+    [ExportGroup("Points of Interest")] [Export]
+    public Npc Romeo;
+
+    [Export] public Npc Juliet;
+    
+    //TODO: West Stairs
+    //TODO: East Stairs
+    //TODO: Balcony
+
+    public void PlayIdentifyTargetsCutscene()
+    {
+        //TODO
+    }
+
+    public void PlaySunRisesInEastCutscene()
+    {
+        //TODO
+    }
+
+    public void PlayMoonRisesInWestCutscene()
+    {
+        //TODO
+    }
+
+    public void PlayEndingCutscene()
+    {
+        //TODO
+    }
+    
+}
