30d16da7baceaab909562c59a80a70e33fa5c7bb
Talberon <developer@talberon.com>
2026-01-30 20:42:22 -0400

    Scaffolding

--- World/Player/Player.cs
+++ World/Player/Player.cs
@@ -3,4 +3,16 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+	[Export] private AnimationPlayer animationPlayer;
+
+	public override void _Ready()
+	{
+		base._Ready();
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		//TODO: Game logic
+		base._PhysicsProcess(delta);
+	}
 }
