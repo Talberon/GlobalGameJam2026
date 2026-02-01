3598d9db03d2aaca2b565667d716ce3be401ae87
Talberon <developer@talberon.com>
2026-02-01 10:18:56 -0400

    wip

--- World/Environment/Eclipse.cs
+++ World/Environment/Eclipse.cs
@@ -3,4 +3,14 @@ using System;
 
 public partial class Eclipse : Node3D
 {
+	[Export] private Node3D preEclipse;
+	[Export] private Node3D postEclipse;
+
+	[Export] private AnimationPlayer sunAnimation;
+
+	public override void _Ready()
+	{
+		sunAnimation.Play("sun");
+		base._Ready();
+	}
 }
